e3edebb69d5c0c12e40d8faa9710df260314a66d
Миронович Владимир <vladimir3211@live.ru>
2021-08-18 12:58:49 +0600

    Lesson 1

---
+++ Lesson 1/1.py
@@ -0,0 +1,4 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print(f'Hi, {name}')
+print(f'Your age: {age}')
---
+++ Lesson 1/2.py
@@ -0,0 +1,19 @@
+print('Instruction:')
+print('Input seconds 1-inf for convert to time')
+print('Input 0 to exit')
+seconds = 1
+while seconds > 0:
+    seconds = int(input('Enter seconds: '))
+
+    h = 0
+    m = 0
+    s = seconds % 60
+
+    result = seconds // 60
+    while result >= 60:
+        h += 1
+        result -= 60
+    else:
+        m = result
+
+    print(f'{h}:{m}:{s}')
